cc920a881a5a0265241204f504bfb3f335e00c2a
Alexis Lar <kevinalexis01mujica@gmail.com>
2026-03-03 18:25:10 -0400

    AGREGAGO NOMBRE

--- src/app/components/avatar/avatar.ts
+++ src/app/components/avatar/avatar.ts
@@ -9,23 +9,43 @@ import { CommonModule } from '@angular/common';
   styleUrl: './avatar.scss',
 })
 export class AvatarComponent {
+    
   colorFondo = signal<string>('#3498db');
   tamano = signal<number>(150);
   tieneLentes = signal<boolean>(false);
   tipoExpresion = signal<number>(1);
 
+  //Manejo de nombre
+  nombreInput = signal<string>(''); 
+  nombreMostrado = signal<string>('');
+
   cambiarColor(event: Event) {
     const input = event.target as HTMLInputElement;
     this.colorFondo.set(input.value);
+    // El nombre cambiará automáticamente en el HTML porque 
+    // usaremos colorFondo() para su estilo.
   }
+
   cambiarTamano(event: Event) {
     const input = event.target as HTMLInputElement;
     this.tamano.set(parseInt(input.value));
   }
+
   toggleLentes() {
     this.tieneLentes.update((flag) => !flag);
   }
+
   setExpresion(tipo: number) {
     this.tipoExpresion.set(tipo);
+  }
+
+  // Métodos para el nombre
+  actualizarInputNombre(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.nombreInput.set(input.value);
+  }
+
+  generarNombre() {
+    this.nombreMostrado.set(this.nombreInput());
   }
 }
